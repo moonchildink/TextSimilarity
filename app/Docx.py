7cf6647357985eff5fba953193ffe2c8ae862794
moonchild <carise0102@gmail.com>
2023-11-16 21:00:48 +0800

    完善了相似度计算、文档类设计

--- app/Docx.py
+++ app/Docx.py
@@ -1,53 +1,116 @@
 import os.path
 from SimhashSimilarity import SimhashSimilarity
 from CosineSimilarity import CosineSimilarity
-import docx
+from bs4 import BeautifulSoup
+import mammoth
 from flask import current_app
 
 
 class Document:
     def __init__(self, file_path):
+        self.input_path = file_path
+        self.output_html_path = os.path.join(current_app.config['TEMP_FILE_DIR'],
+                                             self.input_path.rsplit('\\', 1)[-1].split('.')[0] + '.html')
+        self.output_md_path = self.output_html_path.rsplit('.', 1)[0] + '.md'
         try:
-            self.graphs = []
-            self.file = docx.Document(os.path.join(current_app.config['UPLOAD_FOLDER'], file_path))
-            self.filename = file_path
-            for graph in self.file.paragraphs:
-                self.graphs.append(graph.text)
-            for table in self.file.tables:
-                for row in table.rows:
-                    for cell in row.cells:
-                        self.graphs.append(cell.text)
-        except FileNotFoundError:  # 文件没找到Error该如何处理呢？这肯定会500 Error
+            self.convert_docx_to_html()
+            self.convert_docx_to_markdown()
+        except FileNotFoundError as e:
             pass
+        except Exception as e:
+            pass
+
+    def convert_docx_to_html(self):
+        custom_style = 'b=>i'
+        with open(self.input_path, 'rb') as docx_file:
+            result = mammoth.convert_to_html(docx_file, style_map=custom_style)
+
+        text = result.value
+        with open(self.output_html_path, 'w+', encoding='utf-8') as html_file:
+            html_file.write(text)
+
+    def convert_docx_to_markdown(self):
+        with open(self.input_path, 'rb') as docx_file:
+            result = mammoth.convert_to_markdown(docx_file)
+        with open(self.output_md_path, 'w+', encoding='utf-8') as markdown_file:
+            markdown_file.write(result.value)
 
     @property
     def text(self):
         """
         :return:all texts in document,in the format of list
         """
-        return self.graphs
+        try:
+            with open(self.output_html_path, 'r', encoding='utf-8') as file:
+                html_content = file.read()
+        except FileNotFoundError as e:
+            pass
+        soup = BeautifulSoup(html_content, 'html.parser')
+        elements = soup.find_all(['p', 'li', 'label'])
+        texts = [element.get_text().strip() for element in elements]
+        main_text = '\n'.join(texts)
+
+        return main_text
 
 
 class Documents:
     """
     计算文本之间的相似度方法还有很多，还可以再添加其他的计算方法。
     """
+
     def __init__(self, documents_list):
+        """
+        :param documents_list: 传入文件名列表
+        """
+        self.sorted_simhash_dict = None
+        self.sorted_cosine_dict = None
         self.documents = documents_list
+        self.file_path = [os.path.join(current_app.config['UPLOAD_FOLDER'], document.input_path) for document in
+                          self.documents]
+        self.dic = dict()
+
+        for idx, elem in enumerate(self.documents):
+            temp = Document(self.file_path[idx])
+            self.dic[elem] = temp.text
+
         self.simhash_dict = dict()
         self.get_simhash_similarity()
 
+        self.cosine_dict = dict()
+        self.get_cosine_similarity()
+
+        self.sorted_cosine_dict = sorted(self.cosine_dict.items(), key=lambda item: item[1], reverse=True)
+        self.sorted_simhash_dict = sorted(self.simhash_dict.items(), key=lambda item: item[1], reverse=True)
+
     def get_simhash_similarity(self):
-        for file1 in self.documents:
-            for file2 in self.documents:
-                if file1.filename != file2.filename:
-                    # 在此处求相似度
-                    simhashs = SimhashSimilarity(file1.text, file2.text)
-                    key = str(zip(file1.filename,file2.filename))
-                    self.simhash_dict[key] = simhashs
+        for key, value in self.dic.items():
+            for key2, value2 in self.dic.items():
+                if key != key2:
+                    dict_key = str(zip(key, key2))
+                    self.simhash_dict[dict_key] = SimhashSimilarity(value, value2)
+
+    def get_cosine_similarity(self):
+        for key, value in self.dic.items():
+            for key2, value2 in self.dic.items():
+                if key != key2:
+                    dict_key = str(zip(key, key2))
+                    self.cosine_dict[dict_key] = CosineSimilarity(value, value2)
+
+    def get_simhash_ranked_list(self):
+        return self.sorted_simhash_dict
+
+    def get_cosine_ranked_list(self):
+        return self.sorted_cosine_dict
+
+    @property
+    def get_most_similar(self):
+        first_key, first_value = next(iter(self.cosine_dict.items()))
+        return first_key, first_value
 
     @property
     def simhash_similarity(self):
         return self.simhash_dict
 
-
+    @property
+    def cosine_similarity(self):
+        return self.cosine_dict
